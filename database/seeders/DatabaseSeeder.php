07b2f323a509d525fed1cd7f27341c3c10f5a5b3
Keziah Taganait <keyprocess1982@gmail.com>
2025-04-10 20:09:37 +0800

    Update DatabaseSeeder to include default admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'admin@example.com',
+            'email' => 'markdavidjanapin@gmail.com',
         ]);
     }
 }
